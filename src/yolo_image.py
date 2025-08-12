84cad0c9bf2332c54dbb9fe61b7a5ee65e8d6761
test <49738516+dlsgh2590@users.noreply.github.com>
2025-08-12 13:03:27 +0900

    yolo image

---
+++ src/yolo_image.py
@@ -0,0 +1,17 @@
+from ultralytics import YOLO
+
+# yolo모델 설정
+model = YOLO('yolo11n.pt')
+
+#results = model('https://ultralytics.com/images/bus.jpg')
+
+test_images = [
+    'https://ultralytics.com/images/zidan.jpg'
+    'https://ultralytics.com/images/bus.jpg'
+]
+
+for img in test_images:
+    results = model(img)
+    print(f"검출된 객체 수 : {len(results[0].boxes)}")
+
+results[0].show()
